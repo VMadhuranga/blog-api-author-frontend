be0124e5c363bbfc18e9f2331413d3303a08c0c3
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-21 14:13:25 +0530

    Add initial component elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import "./App.css";
 
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <header>
+        <h1>Blog API</h1>
+      </header>
+      <main>
+        <section>
+          <h2>Login author</h2>
+          <form>
+            <div>
+              <label htmlFor="user_name">User name: </label>
+              <input type="text" name="user_name" id="user_name" />
+            </div>
+            <div>
+              <label htmlFor="password">Password: </label>
+              <input type="text" name="password" id="password" />
+            </div>
+          </form>
+        </section>
+      </main>
+      <footer>
+        <p>&copy; 2024 VMadhuranga</p>
+      </footer>
+    </>
+  );
 }
 
 export default App;
