eddff8f7c7ba708b7e24f22f8db0745da436c21f
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-22 11:08:53 +0530

    Add AuthorLogin component App component's children array
    Additional:
    - Add login author action to AuthorLogin component

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  redirect,
+} from "react-router-dom";
 import App from "./App.jsx";
-import ErrorPage from "./error-page/ErrorPage.jsx";
+import ErrorPage from "./components/error-page/ErrorPage.jsx";
+import AuthorLogin from "./components/author-login/AuthorLogin.jsx";
+import authorLoginAction from "./actions/author-login-action.js";
 import "./index.css";
 
 const router = createBrowserRouter([
@@ -10,6 +16,23 @@ const router = createBrowserRouter([
     path: "/author",
     element: <App />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <AuthorLogin />,
+        action: async ({ request }) => {
+          const formData = await request.formData();
+          const formDataObj = Object.fromEntries(formData);
+          const error = await authorLoginAction(formDataObj);
+
+          if (error) {
+            return error;
+          }
+
+          return redirect("/author/posts");
+        },
+      },
+    ],
   },
 ]);
 
