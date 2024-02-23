2944fc1c4065d567a16ad490c8a6b238b719d7b3
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-23 09:40:59 +0530

    Conditionally render ContentGenerator component
    Conditionally render ContentGenerator component when Create new post
    button clicks

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,27 +1,45 @@
+import { useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
+import ContentGenerator from "../content-generator/ContentGenerator";
 
 export default function Posts() {
+  const [isNewPost, setIsNewPost] = useState(false);
   const posts = useLoaderData();
+
+  function handleCreateNewPost() {
+    setIsNewPost(!isNewPost);
+  }
 
   return (
     <section>
-      <h1>Posts</h1>
-      <article>
-        {posts.length ? (
-          <ul>
-            {posts.map((post) => (
-              <li key={post._id}>
-                <Link to={`${post._id}`}>
-                  {post.title}
-                  <span>{new Date(post.createdDate).toLocaleDateString()}</span>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>There are no posts yet</p>
-        )}
-      </article>
+      <h1>{isNewPost ? "New post" : "Posts"}</h1>
+      {isNewPost ? (
+        <ContentGenerator />
+      ) : (
+        <article>
+          {posts.length ? (
+            <ul>
+              {posts.map((post) => (
+                <li key={post._id}>
+                  <Link to={`${post._id}`}>
+                    {post.title}
+                    <span>
+                      {new Date(post.createdDate).toLocaleDateString()}
+                    </span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>There are no posts yet</p>
+          )}
+        </article>
+      )}
+      <div>
+        <button onClick={handleCreateNewPost} type="button">
+          {isNewPost ? "Cancel" : "Create new post"}
+        </button>
+      </div>
     </section>
   );
 }
