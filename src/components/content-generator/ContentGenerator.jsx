4a305d19c0f15e8d60a6f69c3e30bdc13e5f520a
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-23 14:02:21 +0530

    Use action data to display form fields error messages

--- src/components/content-generator/ContentGenerator.jsx
+++ src/components/content-generator/ContentGenerator.jsx
@@ -1,26 +1,45 @@
+import { Form, useActionData } from "react-router-dom";
+
 export default function ContentGenerator() {
+  const errors = useActionData();
+
   return (
-    <form>
+    <Form method="post">
       <div>
         <label htmlFor="title">Title :</label>
-        <input type="text" name="title" id="title" />
+        <input type="text" name="title" id="title" required />
+        {errors &&
+          errors.data
+            .filter((error) => error.path === "title")
+            .map((error, index) => <span key={index}>{error.msg}</span>)}
       </div>
       <div>
         <label htmlFor="content">Content :</label>
-        <textarea name="content" id="content" cols="30" rows="10"></textarea>
+        <textarea
+          name="content"
+          id="content"
+          cols="30"
+          rows="10"
+          required
+        ></textarea>
+        {errors &&
+          errors.data
+            .filter((error) => error.path === "content")
+            .map((error, index) => <span key={index}>{error.msg}</span>)}
       </div>
       <div>
         <p>Publish :</p>
         <label htmlFor="yes">
-          Yes <input type="radio" name="publish" id="yes" value="yes" />
+          Yes{" "}
+          <input type="radio" name="publish" id="yes" value="yes" required />
         </label>
-        <label htmlFor="yes">
-          No <input type="radio" name="publish" id="no" value="no" />
+        <label htmlFor="no">
+          No <input type="radio" name="publish" id="no" value="no" required />
         </label>
       </div>
       <div>
         <button type="submit">Submit</button>
       </div>
-    </form>
+    </Form>
   );
 }
