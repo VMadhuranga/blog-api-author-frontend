bb3429e94d1c187f8bd75aeb4654bef9aeeaf7b8
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-25 08:20:44 +0530

    Create route for Post component
    - Create route for Post component and add postLoader function
    - Add Comments component to Post component's children array
    - Add commentsLoader and createCommentAction to Comments component

--- src/main.jsx
+++ src/main.jsx
@@ -10,10 +10,15 @@ import ErrorPage from "./components/error-page/ErrorPage.jsx";
 import AuthorLogin from "./components/author-login/AuthorLogin.jsx";
 import Posts from "./components/posts/Posts.jsx";
 import ContentGenerator from "./components/content-generator/ContentGenerator.jsx";
+import Post from "./components/post/Post.jsx";
+import Comments from "./components/comments/Comments.jsx";
 import authorLoginAction from "./actions/author-login-action.js";
 import postsLoader from "./loaders/posts-loader.js";
-import "./index.css";
 import createPostAction from "./actions/create-post-action.js";
+import postLoader from "./loaders/post-loader.js";
+import commentsLoader from "./loaders/comments-loader.js";
+import createCommentAction from "./actions/create-comment-action.js";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
@@ -55,6 +60,33 @@ const router = createBrowserRouter([
               }
 
               return redirect("/author/posts");
+            },
+          },
+        ],
+      },
+      {
+        path: "posts/:post_id",
+        element: <Post />,
+        loader: ({ params }) => {
+          return postLoader(params);
+        },
+        children: [
+          {
+            index: true,
+            element: <Comments />,
+            loader: ({ params }) => {
+              return commentsLoader(params);
+            },
+            action: async ({ params, request }) => {
+              const formData = await request.formData();
+              const formDataObj = Object.fromEntries(formData);
+
+              const errors = await createCommentAction(params, formDataObj);
+              if (errors) {
+                return errors;
+              }
+
+              return redirect(`/author/posts/${params.post_id}`);
             },
           },
         ],
