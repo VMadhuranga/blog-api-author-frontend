9d0bc1ad3bafeb22968674da75b12694326eee2f
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-25 11:00:21 +0530

    Update ContentGenerator component render form fields values

--- src/components/content-generator/ContentGenerator.jsx
+++ src/components/content-generator/ContentGenerator.jsx
@@ -1,13 +1,44 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
 import { Form, useActionData } from "react-router-dom";
 
-export default function ContentGenerator() {
+export default function ContentGenerator({ method = "post", fieldsData }) {
+  const [formFieldsData, setFormFieldsData] = useState(
+    fieldsData || {
+      title: "Post title",
+      content: "Post content",
+      isPublished: false,
+    },
+  );
   const errors = useActionData();
 
+  function handleTitleChange(e) {
+    setFormFieldsData({ ...formFieldsData, title: e.target.value });
+  }
+
+  function handleContentChange(e) {
+    setFormFieldsData({ ...formFieldsData, content: e.target.value });
+  }
+
+  function handlePublishChange(e) {
+    setFormFieldsData({
+      ...formFieldsData,
+      isPublished: e.target.value === "yes",
+    });
+  }
+
   return (
-    <Form method="post">
+    <Form method={method}>
       <div>
         <label htmlFor="title">Title :</label>
-        <input type="text" name="title" id="title" required />
+        <input
+          type="text"
+          name="title"
+          id="title"
+          value={formFieldsData.title}
+          onChange={handleTitleChange}
+          required
+        />
         {errors &&
           errors.data
             .filter((error) => error.path === "title")
@@ -20,6 +51,8 @@ export default function ContentGenerator() {
           id="content"
           cols="30"
           rows="10"
+          value={formFieldsData.content}
+          onChange={handleContentChange}
           required
         ></textarea>
         {errors &&
@@ -31,10 +64,27 @@ export default function ContentGenerator() {
         <p>Publish :</p>
         <label htmlFor="yes">
           Yes{" "}
-          <input type="radio" name="publish" id="yes" value="yes" required />
+          <input
+            type="radio"
+            name="publish"
+            id="yes"
+            value="yes"
+            checked={formFieldsData.isPublished}
+            onChange={handlePublishChange}
+            required
+          />
         </label>
         <label htmlFor="no">
-          No <input type="radio" name="publish" id="no" value="no" required />
+          No{" "}
+          <input
+            type="radio"
+            name="publish"
+            id="no"
+            value="no"
+            checked={!formFieldsData.isPublished}
+            onChange={handlePublishChange}
+            required
+          />
         </label>
       </div>
       <div>
@@ -43,3 +93,8 @@ export default function ContentGenerator() {
     </Form>
   );
 }
+
+ContentGenerator.propTypes = {
+  method: PropTypes.string,
+  fieldsData: PropTypes.object,
+};
