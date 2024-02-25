f8e6045c00f1ce942939d7d4a8f9b78f1b5c7f79
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-25 11:00:59 +0530

    Update Post component to conditionally render ContentGenerator component

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,18 +1,36 @@
+import { useState } from "react";
 import { Link, Outlet, useLoaderData } from "react-router-dom";
+import ContentGenerator from "../content-generator/ContentGenerator";
 
 export default function Post() {
+  const [isEditPost, setIsEditPost] = useState(false);
   const post = useLoaderData();
+
+  function handleEditPost() {
+    setIsEditPost(!isEditPost);
+  }
 
   return (
     <>
       <section>
-        <h2>{post.title}</h2>
-        <p>{new Date(post.createdDate).toLocaleDateString()}</p>
-        <p>{post.isPublished ? "Published" : "Unpublished"}</p>
-        <article>{post.content}</article>
-        <p>
-          See more <Link to={"/author/posts"}>posts</Link>
-        </p>
+        {isEditPost ? (
+          <ContentGenerator method={"put"} fieldsData={post} />
+        ) : (
+          <>
+            <h2>{post.title}</h2>
+            <p>{new Date(post.createdDate).toLocaleDateString()}</p>
+            <p>{post.isPublished ? "Published" : "Unpublished"}</p>
+            <article>{post.content}</article>
+            <p>
+              See more <Link to={"/author/posts"}>posts</Link>
+            </p>
+          </>
+        )}
+        <div>
+          <button onClick={handleEditPost} type="button">
+            {isEditPost ? "Cancel" : "Edit post"}
+          </button>
+        </div>
       </section>
       <Outlet />
     </>
