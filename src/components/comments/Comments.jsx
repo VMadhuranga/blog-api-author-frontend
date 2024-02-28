e59da18fb1718c6b3c39ffbe49b5d91c5e7a2b47
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-28 12:13:55 +0530

    Add Comments component styles

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,14 +1,16 @@
 import { Form, useActionData, useLoaderData } from "react-router-dom";
 import unescape from "../../utils/unescape";
+import styles from "./Comments.module.css";
+import buttonStyles from "../../assets/stylesheets/button.module.css";
 
 export default function Comments() {
   const comments = useLoaderData();
   const errors = useActionData();
 
   return (
-    <section>
+    <section className={styles.commentSection}>
       <h3>Comments</h3>
-      <Form method="post">
+      <Form method="post" className={styles.createCommentForm}>
         <div>
           <label htmlFor="user_name">Name :</label>
           <input type="text" name="user_name" id="user_name" required />
@@ -32,16 +34,18 @@ export default function Comments() {
               .map((error, index) => <span key={index}>{error.msg}</span>)}
         </div>
         <div>
-          <button type="submit">Submit</button>
+          <button type="submit" className={buttonStyles.primary}>
+            Submit
+          </button>
         </div>
       </Form>
       <article>
         {comments.length > 0 ? (
-          <ul>
+          <ul className={styles.commentList}>
             {comments.map((comment) => {
               return (
                 <li key={comment._id}>
-                  <p>{unescape(comment.commentedUser)}</p>
+                  <p>{unescape(comment.commentedUser)} :</p>
                   <p>{unescape(comment.text)}</p>
                   <Form method="delete">
                     <input
@@ -49,14 +53,16 @@ export default function Comments() {
                       name="comment_id"
                       value={comment._id}
                     />
-                    <button type="submit">Delete comment</button>
+                    <button type="submit" className={buttonStyles.primary}>
+                      Delete comment
+                    </button>
                   </Form>
                 </li>
               );
             })}
           </ul>
         ) : (
-          <p>There are no comments yet</p>
+          <p className={styles.noCommentsMsg}>There are no comments yet</p>
         )}
       </article>
     </section>
