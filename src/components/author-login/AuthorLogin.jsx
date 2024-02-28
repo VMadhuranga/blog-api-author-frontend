4e150cd4e8bef9919a49ab987cbeda2805c1d377
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-28 10:46:13 +0530

    Add AuthorLogin component styles

--- src/components/author-login/AuthorLogin.jsx
+++ src/components/author-login/AuthorLogin.jsx
@@ -1,4 +1,6 @@
 import { Form, useActionData } from "react-router-dom";
+import styles from "./AuthorLogin.module.css";
+import buttonStyles from "../../assets/stylesheets/button.module.css";
 
 export default function AuthorLogin() {
   const errors = useActionData();
@@ -6,7 +8,7 @@ export default function AuthorLogin() {
   return (
     <section>
       <h2>Login author</h2>
-      <Form method="post">
+      <Form method="post" className={styles.authorLoginForm}>
         <div>
           <label htmlFor="user_name">User name: </label>
           <input type="text" name="user_name" id="user_name" required />
@@ -24,7 +26,9 @@ export default function AuthorLogin() {
               .map((error, index) => <span key={index}>{error.msg}</span>)}
         </div>
         <div>
-          <button type="submit">Submit</button>
+          <button type="submit" className={buttonStyles.primary}>
+            Submit
+          </button>
         </div>
       </Form>
     </section>
