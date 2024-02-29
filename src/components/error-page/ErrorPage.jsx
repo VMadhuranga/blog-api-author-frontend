0fbe52ed241d40fe777713fffa810fbe5e04cb3d
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-29 09:11:08 +0530

    Conditionally render go back to link when error occurs

--- src/components/error-page/ErrorPage.jsx
+++ src/components/error-page/ErrorPage.jsx
@@ -1,15 +1,22 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 import styles from "./ErrorPage.module.css";
 
 export default function ErrorPage() {
   const error = useRouteError();
+  const errMsg = error.statusText || error.message;
 
   return (
     <div id="error-page" className={styles.errorPage}>
       <h2>Oops!</h2>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{errMsg}</i>
+      </p>
+      <p>
+        Go back to{" "}
+        <Link to={errMsg === "Not Found" ? "/author/posts" : "/author"}>
+          {errMsg === "Not Found" ? "Posts" : "Login"}
+        </Link>
       </p>
     </div>
   );
